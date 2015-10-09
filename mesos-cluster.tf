7399f4b699554fa389fbc39bb170852ad6a9c011
John Giannelos <johngiannelos@gmail.com>
2015-10-09 15:25:26 +0300

    Allow outgoing traffic from all nodes.

--- mesos-cluster.tf
+++ mesos-cluster.tf
@@ -59,6 +59,12 @@ resource "aws_security_group" "mesos-default-sg" {
     cidr_blocks = ["${aws_subnet.mesos-public-subnet.cidr_block}"]
   }
 
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   vpc_id = "${aws_vpc.mesos.id}"
 }

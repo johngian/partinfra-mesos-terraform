38e886d825f5075f849321021b974eaed7d9440a
John Giannelos <johngiannelos@gmail.com>
2015-10-21 13:04:33 +0300

    Allow pings in clusters internal traffic.

--- mesos-cluster.tf
+++ mesos-cluster.tf
@@ -59,6 +59,13 @@ resource "aws_security_group" "mesos-default-sg" {
     cidr_blocks = ["${aws_subnet.mesos-public-subnet.cidr_block}"]
   }
 
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["${aws_subnet.mesos-public-subnet.cidr_block}"]
+  }
+
   egress {
     from_port = 0
     to_port = 0
